45316fceac751793b0efabe2d7bd53dc3d7d07f4
ellaellac <117093750+ellaellac@users.noreply.github.com>
2025-09-13 19:10:26 +0100

    network requests

--- cypress/e2e/intercept_start.cy.ts
+++ cypress/e2e/intercept_start.cy.ts
@@ -1,6 +1,10 @@
-it.only('board has no lists', () => {
-
+it('board has no lists', () => {
+  // handling false positive
+  cy.intercept('GET', '/api/lists?boardId=1').as('getLists')
+// intercept command : wait for a certain API call to be made and wait for its response
   cy.visit('/board/1')
+
+  cy.wait('@getLists')
 
   cy.get('[data-cy=list]')
     .should('not.exist')
@@ -8,13 +12,21 @@ it.only('board has no lists', () => {
 });
 
 it('deleting a list', () => {
+  // put the intercept before API call 
+  cy.intercept({ 
+    method: 'DELETE',
+    url: '/api/lists/1',  // url: '/api/lists/*'
+  }).as('deleteList')
 
   cy.visit('/board/1')
 
-  cy.get('[data-cy="list-options"]')
+  cy.get('[data-testid="list-options"]')
     .click()
 
-  cy.get('[data-cy="delete-list"]')
+  cy.get('[data-testid="delete-list"]')
     .click()
 
+  cy.wait('@deleteList') // return a object of the API call
+    .its('response.statusCode')
+    .should('eq', 200)
 });
