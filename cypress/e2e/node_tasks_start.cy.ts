7b9dd5bf009676ed78ad53e0b4e995a38b8d48e8
ellaellac <117093750+ellaellac@users.noreply.github.com>
2025-09-16 23:26:47 +0100

    seed database

--- cypress/e2e/node_tasks_start.cy.ts
+++ cypress/e2e/node_tasks_start.cy.ts
@@ -1,9 +1,23 @@
 it('resets a database', () => {
 
-  cy.task('seedDatabase', {
-    boards: [], 
-    cards: [], 
-    lists: [], 
+  // cy.task('seedDatabase', {
+  //   boards:[],
+  //   cards: [],
+  //   lists: [],
+  //   users: []
+  // })
+
+  //Ref to seedDatabase.js
+  cy.task('seedDatabase', { //arg : needs to be an object or a single parameter
+    boards: [{
+      name: 'My first board',
+      id: 1,
+      starred: true,
+      user: 0,
+      created: '2025-09-10'
+    }],
+    cards: [{boardId: 1, name: "card", order: 0}],
+    lists: [{boardId: 1, name: "first list", order: 1}],
     users: []
   })
 
